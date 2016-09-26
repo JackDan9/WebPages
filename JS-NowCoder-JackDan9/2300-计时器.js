ff4cce1a2cbdf6c2526830932e2642288cf54792
JackDan9 <1835812864@qq.com>
2016-09-26 18:29:01 +0800

    Add timer script by javascript

---
+++ JS-NowCoder-JackDan9/2300-计时器.js
@@ -0,0 +1,9 @@
+function count(start, end) {  
+    console.log(start)  
+    var timer = setInterval(  
+        function(){  
+            if(start<end) console.log(start+=1);  
+        },100)  
+    return {cancel:function(){clearInterval(timer)}}  
+  
+}  
