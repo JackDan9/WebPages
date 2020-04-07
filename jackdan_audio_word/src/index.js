a9393e8527b51ec0abec323f13d1130fce092d3a
jackdan <1835812864@qq.com>
2020-04-07 13:16:27 +0800

    `jackdan_audio_word` update files.

--- jackdan_audio_word/src/index.js
+++ jackdan_audio_word/src/index.js
@@ -12,10 +12,10 @@ class App extends React.Component {
 
   render() {
     return (
-      <div class="container">
-        <div class="button-wrapper">
-          <div class="button">
-              <svg class="button-svg" viewBox="0 0 24 24">
+      <div className="container">
+        <div className="button-wrapper">
+          <div className="button">
+              <svg className="button-svg" viewBox="0 0 24 24">
                   <path d="M12 15c1.66 0 2.99-1.34 2.99-3l.01-6c0-1.66-1.34-3-3-3s-3 1.34-3 3v6c0 1.66 1.34 3 3 3zm5.3-3c0 3-2.54 5.1-5.3 5.1s-5.3-2.1-5.3-5.1h-1.7c0 3.42 2.72 6.23 6 6.72v3.28h2v-3.28c3.28-.48 6-3.3 6-6.72h-1.7z" />
                   <path d="M0 0h24v24h-24z" fill="none" />
               </svg>
