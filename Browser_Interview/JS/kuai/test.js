8c8a691dd6b39f944b873d5c82288a8f1f87ea9c
JackDan9 <1835812864@qq.com>
2020-07-08 22:18:14 +0800

    Add `kuai` file; Add a series of `jackdan mobx` files.

--- Browser_Interview/JS/kuai/test.js
+++ Browser_Interview/JS/kuai/test.js
@@ -1,37 +1,50 @@
-//line=readline()
-//print(line)
-//console.log('Hello World!');
+/**
+ * fe: 'abcde'
+ * rb: '123456789'
+ * weeks: 20
+ * result: 
+ * @param {*} fe 
+ * @param {*} rb 
+ * @param {*} weeks 
+ */
 function handleZb(fe, rb, weeks) {
-    var feLen = fe.length;
-    var rbLen = rb.length;
-    
+    var feArr = fe.split('');
+    var rbArr = rb.split('');
+    var feLen = feArr.length;
+    var rbLen = rbArr.length;
+
     var indexFeLen = weeks - feLen;
     var indexRbLen = weeks - rbLen;
+
+    var result = [];
     
     while(indexFeLen > feLen) {
-        fe.concat(fe);
-        feLen = fe.length;
-        indexFeLen = weeks - feLen; 
+        feArr = feArr.concat(feArr);
+        feLen = feArr.length;
+        indexFeLen = weeks - feLen;
     }
-    if(indexFeLen <= feLen) {
-        fe.concat(fe.substring(0, indexFeLen));
+
+    if(0 < indexFeLen <= feLen) {
+        feArr = feArr.concat(feArr.slice(0, indexFeLen));
     }
     
     while(indexRbLen > rbLen) {
-        rb.concat(rb);
-        rbLen = rb.length;
+        rbArr = rbArr.concat(rbArr);
+        rbLen = rbArr.length;
         indexRbLen = weeks - rbLen;
     }
     
-    if(indexRbLen <= rbLen) {
-        rb.concat(fe.substring(0, indexRbLen));
+    if(0 < indexRbLen <= rbLen) {
+        rbArr = rbArr.concat(rbArr.slice(0, indexRbLen));
     }
-    // console.log(fe);
-    // console.log(rb);
-    return fe;
-    var result = [];
+
+    for(var i = 0;  i < weeks; i++) {
+        result.push(feArr[i]+rbArr[i])
+    }
+
+    return result;
 }
 
-var fe = handleZb('abcde', '123456789', 20);
-
+var result = handleZb('abcdefghijklmnopquvwxyz', '123456789', 20);
+console.log(result);
 
--- jackdan_mobx/webpack.config.js
+++ jackdan_mobx/webpack.config.js
@@ -1,6 +1,39 @@
 const path = require('path');
 const webpack = require('webpack');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'my-first-mobx-test.bundle.js'
+    },
+    mode: 'development',
+    devServer: {
+        host: '0.0.0.0',
+        hot: true, // 热替换
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true, // gzip压缩
+        port: 8000
+    },
+    devtool: "sourceMap",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            filename: path.resolve(__dirname, 'dist/index.html')
+        })
+    ]
 }
