dbc614ceed288d43a57a84ee2495c41432b8c4fb
jackdan <1835812864@qq.com>
2022-08-28 17:02:30 +0800

    :cn: Update `myBind` function module;

--- Browser_Interview/JS/Bind/test.js
+++ Browser_Interview/JS/Bind/test.js
@@ -1,7 +1,34 @@
 Function.prototype.myBind = function(context) {
   var self = this;
 
-  var context = 
+  var args = Array.prototype.slice.call(arguments, 1);
 
+  var fNOP = function () {};
 
+  var fBound = function () {
+    var bindArgs = Array.prototype.slice.call(arguments);
+    return self.apply(this instanceof fNOP ? this : context, args.concat(bindArgs));
+  }
+
+  fNOP.prototype = this.prototype;
+  fBound.prototype = new fNOP();
+
+  return fBound;
 }
+
+
+function LaterBooler() {
+  this.count = 1;
+}
+
+LaterBooler.prototype.bloom = function () {
+  global.setTimeout(this.declare.myBind(this), 2000);
+}
+
+LaterBooler.prototype.declare = function () {
+  console.log('bobo called number is: ' + this.count + ' times');
+}
+
+var boomer = new LaterBooler;
+
+boomer.bloom();
